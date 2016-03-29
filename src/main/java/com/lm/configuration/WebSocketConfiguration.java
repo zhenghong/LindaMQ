4340a2f17ec6057599eef923d6758fa4f42667b2
zhenghong <zhenghong596@126.com>
2016-03-29 23:14:52 +0800

    modify

--- src/main/java/com/lm/configuration/WebSocketConfiguration.java
+++ src/main/java/com/lm/configuration/WebSocketConfiguration.java
@@ -1,6 +1,5 @@
 package com.lm.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
